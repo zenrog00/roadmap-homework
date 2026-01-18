0bd0ea233b297a0288ae5676c1dd1c8428b98ff6
Ilya <>
2026-01-18 16:51:41 +0300

    Added APP_PORT env variable to configure application port

--- roadmap-homework/src/env/environment-variables.ts
+++ roadmap-homework/src/env/environment-variables.ts
@@ -1,13 +1,26 @@
-import { IsNotEmpty, IsUrl, IsNumber, Min, Max } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsUrl,
+  IsNumber,
+  Min,
+  Max,
+  IsOptional,
+} from 'class-validator';
 
 export class EnvironmentVariables {
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Max(65535)
+  APP_PORT: number;
+
   @IsNotEmpty()
   @IsUrl({ require_tld: false })
   POSTGRES_HOST: string;
 
   @IsNumber()
   @Min(0)
-  @Max(65536)
+  @Max(65535)
   POSTGRES_PORT: number;
 
   @IsNotEmpty()
--- roadmap-homework/src/main.ts
+++ roadmap-homework/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
