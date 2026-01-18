1ac9191031db16ea4ac4e750a13ecab9a473b50f
Ilya <>
2026-01-18 16:03:49 +0300

    Removed default AppController and AppService

--- roadmap-homework/src/main.ts
+++ roadmap-homework/src/main.ts
@@ -5,4 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
