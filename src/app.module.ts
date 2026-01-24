f4ebafa24c6b32f82293b89ce7e504a1723913e1
Ilya <>
2026-01-24 09:17:22 +0300

    Added and configured typeorm-transactional package

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './database/typeorm-config-service';
 import { AuthModule, AuthModuleOptionsFactory } from './auth';
 import { UsersModule } from './users/users.module';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate }),
-    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      // eslint-disable-next-line @typescript-eslint/require-await
+      dataSourceFactory: async (options) => {
+        // options are validated with env-validation, therefore using !
+        return addTransactionalDataSource(new DataSource(options!));
+      },
+    }),
     AuthModule.forRootAsync({ useClass: AuthModuleOptionsFactory }),
     UsersModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  initializeTransactionalContext,
+  StorageDriver,
+} from 'typeorm-transactional';
 
 async function bootstrap() {
+  initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.APP_PORT ?? 3000);
 }
