20808ac8822f4a8fd2b676a9b5707c09dac74c26
Ilya <>
2026-01-31 14:30:41 +0300

    Added POST /auth/register tests for valid input data

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import {
   PostgreSqlContainer,
   StartedPostgreSqlContainer,
@@ -11,10 +11,15 @@ import {
   initializeTransactionalContext,
   StorageDriver,
 } from 'typeorm-transactional';
+import { GetUsersResponseDto, UserDto } from 'src/users/dtos';
+import * as cookie from 'cookie';
 
 let app: INestApplication;
 let api: AxiosInstance;
 let postgresContainer: StartedPostgreSqlContainer;
+
+const uuidRegex =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 beforeAll(async () => {
   postgresContainer = await new PostgreSqlContainer('postgres:18').start();
@@ -58,13 +63,99 @@ afterAll(async () => {
   await postgresContainer.stop();
 });
 
-describe('api', () => {
-  describe('GET /', () => {
-    it('should return 404 on / route', async () => {
-      const response = await api.get('/');
+describe('AUTH', () => {
+  let userDto: UserDto;
 
-      expect(response).toMatchObject({
-        status: 404,
+  beforeEach(() => {
+    userDto = {
+      username: `user_${Date.now()}`,
+      email: `${Date.now()}@yahoo.com`,
+      password: 'test_pass',
+      birthdate: new Date(),
+      description: '',
+    };
+  });
+
+  describe('POST /auth/register', () => {
+    let response: AxiosResponse;
+
+    beforeEach(async () => {
+      response = await api.post('/auth/register', userDto);
+    });
+
+    describe('valid user data', () => {
+      it('should return 201 response code', () => {
+        expect(response.status).toBe(201);
+      });
+
+      it('should return JWT accessToken', () => {
+        expect(response.data).toHaveProperty('accessToken');
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        expect(typeof response.data.accessToken).toBe('string');
+
+        const jwtRegex = /^[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+\.[a-zA-Z0-9-_]+$/;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        expect(response.data.accessToken).toMatch(jwtRegex);
+      });
+
+      it('should return HttpOnly refreshToken cookie with path /auth', () => {
+        const rawCookie = response.headers['set-cookie']?.[0];
+        expect(rawCookie).toBeDefined();
+        // cookie.parse doesn't parse httpOnly because it doesn't have value
+        expect(rawCookie!.toLowerCase()).toContain('httponly');
+
+        const parsedCookie = cookie.parse(rawCookie!);
+        expect(typeof parsedCookie.refreshToken).toEqual('string');
+        expect(parsedCookie.Path).toEqual('/auth');
+        expect(parsedCookie.Expires).toBeDefined();
+        expect(new Date(parsedCookie.Expires!).getTime()).toBeGreaterThan(
+          Date.now(),
+        );
+
+        expect(parsedCookie.refreshToken).toMatch(uuidRegex);
+      });
+
+      it('should save user to database', async () => {
+        const userResponse = await api.get<GetUsersResponseDto>('users', {
+          params: {
+            username: userDto.username,
+          },
+          headers: {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            Authorization: `Bearer ${response.data.accessToken}`,
+          },
+        });
+        const userData = userResponse.data.data[0];
+        expect(userData).toEqual({
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          id: expect.stringMatching(uuidRegex),
+          username: userDto.username,
+          email: userDto.email,
+          birthdate: userDto.birthdate.toISOString().split('T')[0],
+          description: userDto.description,
+        });
+      });
+
+      it('should not register users with same username', async () => {
+        const repeatedUsernameResponse = await api.post('/auth/register', {
+          ...userDto,
+          email: `${Date.now()}@yahoo.com`,
+        });
+        expect(repeatedUsernameResponse.data).toMatchObject({
+          statusCode: 400,
+          message: `Username or email already exists!`,
+        });
+      });
+
+      it('should not register users with same email', async () => {
+        const repeatedEmailResponse = await api.post('/auth/register', {
+          ...userDto,
+          username: `user_${Date.now()}`,
+        });
+        expect(repeatedEmailResponse.data).toMatchObject({
+          statusCode: 400,
+          message: `Username or email already exists!`,
+        });
       });
     });
   });
