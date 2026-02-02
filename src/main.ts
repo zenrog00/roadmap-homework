caf798bd5815eeff8eb27c08cc0f27cb9960f200
Ilya <>
2026-02-02 12:20:01 +0300

    Added and configured @nestjs/swagger with cli plugin

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,24 @@ import {
   StorageDriver,
 } from 'typeorm-transactional';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
+
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Roadmap homework')
+    .setDescription('Roadmap homework API description')
+    .setVersion('0.0.1')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory, {
+    customSiteTitle: 'Roadmap homework API',
+  });
+
   app.use(cookieParser());
   await app.listen(process.env.APP_PORT ?? 3000);
 }
