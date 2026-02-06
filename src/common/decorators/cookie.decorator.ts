8b3945b4cc326ea605000f3bff58090709a4001d
Ilya <>
2026-02-06 19:32:00 +0300

    Added cookies type assertion to fix eslint error

--- src/common/decorators/cookie.decorator.ts
+++ src/common/decorators/cookie.decorator.ts
@@ -4,8 +4,7 @@ import { Request } from 'express';
 export const Cookie = createParamDecorator(
   (name: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<Request>();
-    const cookies = request.cookies;
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    const cookies = request.cookies as Record<string, string>;
     return name ? cookies?.[name] : cookies;
   },
 );
