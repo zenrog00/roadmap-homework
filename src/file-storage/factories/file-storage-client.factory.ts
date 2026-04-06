66f1920796ba1dea19f529dc879fa74534ce61ac
Ilya <>
2026-04-06 15:51:01 +0300

    Implemented factory function to create file-storage clients; improved typing strictness

--- src/file-storage/factories/file-storage-client.factory.ts
+++ src/file-storage/factories/file-storage-client.factory.ts
@@ -1,5 +1,8 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import { FileStorageDriver } from '../interfaces/file-storage.options';
+import {
+  FileStorageDriver,
+  FileStorageOptionsByDriver,
+} from '../interfaces/file-storage.options';
 
 type FileStorageClientByDriverMap = {
   s3: S3Client;
@@ -10,3 +13,22 @@ export type FileStorageClientByDriver<D extends FileStorageDriver> =
   // using FileStorageClientByDriverMap to get strong typing
   // about when to pass client to storage constructor
   FileStorageClientByDriverMap[D];
+
+type FileStorageClientCreatorByDriver = {
+  [D in FileStorageDriver]: (
+    options: FileStorageOptionsByDriver<D>,
+  ) => FileStorageClientByDriver<D>;
+};
+
+const FILE_STORAGE_CLIENT_CREATORS: FileStorageClientCreatorByDriver = {
+  s3: (options) => new S3Client(options),
+  disk: () => {
+    throw new Error('Disk storage does not require a client');
+  },
+};
+
+export function createFileStorageClient<D extends FileStorageDriver>(
+  options: FileStorageOptionsByDriver<D>,
+): FileStorageClientByDriver<D> {
+  return FILE_STORAGE_CLIENT_CREATORS[options.driver](options);
+}
