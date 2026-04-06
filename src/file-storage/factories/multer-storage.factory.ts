7aa21958dd1d258760973bc9a8fc9e2433772a95
Ilya <>
2026-04-06 18:26:12 +0300

    Implemented factory function for creating multer storages

---
+++ src/file-storage/factories/multer-storage.factory.ts
@@ -0,0 +1,48 @@
+import type { StorageEngine } from 'multer';
+import type { FileStorageDriver } from '../interfaces/file-storage.options';
+import { s3Storage } from '../storages/s3/multer/s3-multer-storage';
+import type { S3MulterStorageOptions } from '../storages/s3/multer/s3-multer-storage.options';
+import type { FileStorageClientByDriver } from './file-storage-client.factory';
+
+type MulterStorageOptionsByDriverMap = {
+  s3: Omit<S3MulterStorageOptions, 's3Client'>;
+  disk: never;
+};
+
+export type MulterOptionsByDriver<D extends FileStorageDriver> =
+  MulterStorageOptionsByDriverMap[D];
+
+type MulterStorageDepByDriverMap = {
+  s3: FileStorageClientByDriver<'s3'>;
+  disk: never;
+};
+
+type MulterStorageDepByDriver<D extends FileStorageDriver> =
+  MulterStorageDepByDriverMap[D];
+
+type MulterStorageDepArg<D extends FileStorageDriver> =
+  MulterStorageDepByDriver<D> extends never
+    ? []
+    : [dep: MulterStorageDepByDriver<D>];
+
+type MulterStorageCreatorByDriver = {
+  [D in FileStorageDriver]: (
+    options: MulterOptionsByDriver<D>,
+    ...depArg: MulterStorageDepArg<D>
+  ) => StorageEngine;
+};
+
+const MULTER_STORAGE_CREATORS: MulterStorageCreatorByDriver = {
+  s3: (options, s3Client) => s3Storage({ ...options, s3Client }),
+  disk: () => {
+    throw new Error('Disk multer storage is not implemented');
+  },
+};
+
+export function createMulterStorage<D extends FileStorageDriver>(
+  driver: D,
+  options: MulterOptionsByDriver<D>,
+  ...depArg: MulterStorageDepArg<D>
+): StorageEngine {
+  return MULTER_STORAGE_CREATORS[driver](options, ...depArg);
+}
