5661b40289b3999be8db820e011891e4e35656fc
Ilya <>
2026-04-08 17:50:04 +0300

    Added UserAvatar entity for many-to-many relationship

---
+++ src/users/avatars/entities/user-avatar.ts
@@ -0,0 +1,26 @@
+import { User } from 'src/users/entities';
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Avatar } from './avatar.entity';
+
+@Entity({ name: 'user_avatars' })
+export class UserAvatar {
+  @PrimaryColumn('uuid')
+  user_id: string;
+
+  @ManyToOne(() => User, {
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @PrimaryColumn('uuid', { unique: true })
+  avatarId: string;
+
+  @ManyToOne(() => Avatar, {
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({ name: 'avatarId' })
+  avatar: Avatar;
+}
