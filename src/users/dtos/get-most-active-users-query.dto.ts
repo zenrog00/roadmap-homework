e20c879cad07cf9f523096a4db813c3a0abb1f23
Ilya <>
2026-04-10 19:43:26 +0300

    Added dto class for get most active users request query

---
+++ src/users/dtos/get-most-active-users-query.dto.ts
@@ -0,0 +1,31 @@
+import { ApiPropertyOptional, IntersectionType } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
+import { CursorPaginationQueryDto } from 'src/common/dtos';
+
+class MostActiveUsersAgeQueryDto {
+  @ApiPropertyOptional({ description: 'Minimum age (inclusive)', example: 18 })
+  @IsOptional()
+  @Transform(({ value }) =>
+    value === undefined || value === '' ? undefined : Number(value),
+  )
+  @IsInt()
+  @Min(0)
+  @Max(150)
+  ageFrom?: number;
+
+  @ApiPropertyOptional({ description: 'Maximum age (inclusive)', example: 65 })
+  @IsOptional()
+  @Transform(({ value }) =>
+    value === undefined || value === '' ? undefined : Number(value),
+  )
+  @IsInt()
+  @Min(0)
+  @Max(150)
+  ageTo?: number;
+}
+
+export class GetMostActiveUsersQueryDto extends IntersectionType(
+  MostActiveUsersAgeQueryDto,
+  CursorPaginationQueryDto,
+) {}
--- src/users/dtos/index.ts
+++ src/users/dtos/index.ts
@@ -2,3 +2,4 @@ export { UserDto } from './user.dto';
 export { GetUsersQueryDto } from './get-users-query.dto';
 export { UserResponseDto } from './user-response.dto';
 export { GetUsersResponseDto } from './get-users-response.dto';
+export { GetMostActiveUsersQueryDto } from './get-most-active-users-query.dto';
