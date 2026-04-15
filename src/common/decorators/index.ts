6f27b736bef1d92f4e5dd3cd5e1ae878e96272c4
Ilya <>
2026-04-15 18:52:52 +0300

    Added custom decorator to get idempotency key from request headers

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -1,3 +1,4 @@
 export { User } from './user.decorator';
 export { Cookie } from './cookie.decorator';
 export { Fingerprint } from './fingerprint.decorator';
+export { IdempotencyKey } from './idempotency-key.decorator';
