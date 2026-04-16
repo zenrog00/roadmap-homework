bf97d233870c4797ba015bae8c94a404c824f965
Ilya <>
2026-04-16 18:58:53 +0300

    Added body dto for balance transfer operation

--- src/users/balance/dtos/index.ts
+++ src/users/balance/dtos/index.ts
@@ -1,2 +1,3 @@
 export { BalanceOperationDto } from './balance-operation.dto';
 export { BalanceResponseDto } from './balance-response.dto';
+export { BalanceTransferDto } from './balance-transfer.dto';
